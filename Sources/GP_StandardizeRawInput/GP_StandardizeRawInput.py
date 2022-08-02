135110d4effbc6fc63a44420dcda0517c3e34b50
vovetskyy <vovetskyy@gmail.com>
2022-08-02 12:42:40 +0200

    - logging config moved to main()  - removed test-function

--- Sources/GP_StandardizeRawInput/GP_StandardizeRawInput.py
+++ Sources/GP_StandardizeRawInput/GP_StandardizeRawInput.py
@@ -1,14 +1,9 @@
 import logging
 import GP_StandardizeRawIPG as ipg
 
-logging.basicConfig(level=logging.DEBUG, format=' %(asctime)s - %(levelname)s - %(message)s')
-
-
-def test():
-    print('K')
-    pass
-
 
 if __name__ == "__main__":
+    logging.basicConfig(level=logging.DEBUG, format=' %(asctime)s - %(levelname)s - %(message)s')
     logging.info('start')
+
     ipg.standardize_raw_IPG(r'c:\Wit\Scripts\GreenParrot\FastShot1\RawOutput\DESKTOP-FP4OP26\DESKTOP-FP4OP26__2022-07-14_15-28-42__IPG.csv')
