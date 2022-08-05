c4cfdf4103117096c239f7897a621f4fe4b0493c
vovetskyy <vovetskyy@gmail.com>
2022-08-05 20:22:05 +0200

    - added input/output dirs as command-line options

--- Sources/GP_StandardizeRawInput/GP_StandardizeRawInput.py
+++ Sources/GP_StandardizeRawInput/GP_StandardizeRawInput.py
@@ -1,22 +1,42 @@
+import argparse
 import logging
 from pathlib import Path
 from pprint import pprint as pp
 import GP_StandardizeRawIPG as ipg
 
+DEF_OUT_DIR = '__STD_RAW_OUTPUT'
 
 if __name__ == "__main__":
+    # let's start with logging
     logging.basicConfig(level=logging.DEBUG, format=' %(asctime)s - %(levelname)s - %(message)s')
-    logging.info('start')
+    logging.info('Start GP raw input standardization')
 
-    parsing_dir = r'c:\Wit\Scripts\GreenParrot\FastShot1\RawOutput\DESKTOP-FP4OP26'
-    out_dir = r'c:\Wit\Scripts\GreenParrot\__STD_OUTPUT'
+    # parse command-line options
+    cmd_parser = argparse.ArgumentParser(description='Standardization of GP raw input files')
+    cmd_parser.add_argument('--indir', help='Directory to parse for raw input. By default -- current_dir',
+                            default=str(Path.cwd()))
+    cmd_parser.add_argument('--outdir',
+                            help='Directory to store result of the parsing. By default  -- current_dir\\' + DEF_OUT_DIR,
+                            default=str(Path(Path.cwd(), DEF_OUT_DIR)))
+    cmd_parser.print_help()  # print it anyway as user-friendly hint
 
+    cmd_args = cmd_parser.parse_args()
+
+    # parsing_dir = r'c:\Wit\Scripts\GreenParrot\FastShot1\RawOutput\DESKTOP-FP4OP26'
+    parsing_dir = cmd_args.indir
+    # out_dir = r'c:\Wit\Scripts\GreenParrot\__STD_OUTPUT'
+    out_dir = cmd_args.outdir
+
+    logging.info('Start parsing of "' + parsing_dir + '"')
+    logging.info('Results will be stored to  "' + out_dir + '"')
+
+    # parse IPG raw inputs
     parse_path = Path(parsing_dir)
     file_list = (list(parse_path.glob('*__IPG.*')))
-    # pp(file_list[0])
 
     for file in file_list:
         ipg.standardize_raw_IPG(str(file), out_dir)
+        # pp(str(file))
 
     # ipg.standardize_raw_IPG(r'c:\Wit\Scripts\GreenParrot\FastShot1\RawOutput\DESKTOP-FP4OP26\DESKTOP-FP4OP26__2022-07-31_12-16-45__IPG.csv',
-    #                        out_dir)
+    #                       out_dir)
