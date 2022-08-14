c57aa68de47304416759811be9a5b088cc6a564d
vovetskyy <vovetskyy@gmail.com>
2022-08-14 08:34:59 +0200

    - added searching for files using common constants

--- Sources/GP_StandardizeRawInput/GP_StandardizeRawScript2.py
+++ Sources/GP_StandardizeRawInput/GP_StandardizeRawScript2.py
@@ -7,24 +7,26 @@ from pprint import pprint as pp
 import GP_RawInputUtils as rawu
 
 
-def standardize_raw_Script2_file(full_filename:str, out_dir:str):
+def standardize_raw_Script2_file(full_filename: str, out_dir: str):
     """
     converts to std format the json-files, created in the format of the script
     https://github.com/vovetskyy/GP_FastShot1/blob/0aa893d3a6ab1badd33bcb735aee5e09351f960e/main.py
 
     :param full_filename: string with full name (including full path) of the raw IPG file
-    :param out_dir: string with full path to the ditectory to store resulting file(s)
+    :param out_dir: string with full path to the directory to store resulting file(s)
     :return: None
     """
     logging.info('Start handling of file ' + '"' + full_filename + '"')
 
     with open(full_filename) as json_file:
         json_dict = json.load(json_file)
-    # pp(json_dict.keys())
 
+    # as dict keys are not mandatory sorted, get timestamps sorted by time
+    times_list = list(json_dict.keys())
+    times_list.sort()
+    # pp(type(json_dict[times_list[0]]))
 
-
-def standardize_raw_Script2_in_dir(parsing_dir, out_dir):
+def standardize_raw_Script2_in_dir(parsing_dir: str, out_dir: str):
     """
     finds all raw Script2 files in parsing_dir and stores standardized files in out_dir
     :param parsing_dir:
@@ -34,7 +36,7 @@ def standardize_raw_Script2_in_dir(parsing_dir, out_dir):
     logging.info('Start standardization of raw Script2 files from "' + str(parsing_dir) + '" to "' + str(out_dir) + '"')
 
     parse_path = Path(parsing_dir)
-    file_list = (list(parse_path.glob('*__Script2.*')))
+    file_list = (list(parse_path.glob('*' + rawu.RAW_FILENAME_DELIM + rawu.RAW_SCRIPT2_FILENAME_SUFFIX + '.*')))
 
     for file in file_list:
         # standardize_raw_Script2_file(str(file), out_dir)
